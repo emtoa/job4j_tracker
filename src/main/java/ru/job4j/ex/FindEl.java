d5371d51379586b325f94621bf98b14630f7d0ea
emtoa <emtoa@yandex.ru>
2020-06-20 11:51:31 +0300

    0.4. Пользовательские исключения. [#297955]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,29 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for(int i = 0; i < value.length; i++){
+            if (value[i] == key){
+                rsl = i;
+                break;
+            }
+        }
+
+        if (rsl == -1){
+            throw new ElementNotFoundException ("The item is not found !!!");
+        }
+
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            String[] str = new String[]{"zero","first","second","third"};
+
+            indexOf(str, "second");
+        } catch (ElementNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+}
