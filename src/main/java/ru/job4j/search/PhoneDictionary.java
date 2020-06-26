a4494c1e38e9dd7850ad04a70016a9bcecafc6a4
emtoa <emtoa@yandex.ru>
2020-06-27 00:04:20 +0300

    1. Телефонный справочник на базе ArrayList [#297913]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,30 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     * @param key Ключ поиска.
+     * @return Список подощедщих пользователей.
+     */
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<Person>();
+        for (int i = 0; i < this.persons.size(); i++) {
+            if ((this.persons.get(i).getName().contains(key)) ||
+                    (this.persons.get(i).getSurname().contains(key)) ||
+                    (this.persons.get(i).getPhone().contains(key)) ||
+                    (this.persons.get(i).getAddress().contains(key)))
+            {
+                result.add(this.persons.get(i));
+            }
+        }
+        return result;
+    }
+}
