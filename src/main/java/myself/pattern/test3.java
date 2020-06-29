523ff502bbefc77fb8fc1aa8e618ced6ec0e7897
emtoa <emtoa@yandex.ru>
2020-06-29 15:17:12 +0300

    test

---
+++ src/main/java/myself/pattern/test3.java
@@ -0,0 +1,17 @@
+package myself.pattern;
+
+public class test3 {
+    public static void main(String[] args) {
+        int[][] intarr = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+        for (int[] arr : intarr){
+            for (int value : arr){
+                System.out.println(value);
+            }
+        }
+    }
+}
