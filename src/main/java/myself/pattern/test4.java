415ecb220999f54eea2190b520ed80699e331ed0
emtoa <emtoa@yandex.ru>
2020-07-01 02:58:51 +0300

    test

--- src/main/java/myself/pattern/test4.java
+++ src/main/java/myself/pattern/test4.java
@@ -1,19 +1,34 @@
 package myself.pattern;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import ru.job4j.search.*;
+import ru.job4j.tracker.Item;
 
 public class test4 {
 
     public static void main(String[] args) {
 
+        List<Item> lst1 = new ArrayList<Item>();
+            lst1.add(new Item("Sasha"));
+            lst1.add(new Item("Masha"));
+            lst1.add(new Item("Glash"));
 
-        ArrayList<Person> lPerson = new ArrayList<Person>();
+//        List<Item> lst = Arrays.asList(new Item("Sasha"));
+//        System.out.println(lst.get(0).getName());
 
-        Person piople = new Person("Alex","Kharitonov","783423","v jope");
+        for (Item value : lst1) {
+            System.out.println(value.getName());
+        }
 
-        lPerson.add(piople);
-
-        System.out.println(lPerson.get(0).getName());
+        lst1.remove(0);
+        System.out.println("##########################");
+        for (Item value : lst1) {
+            System.out.println(value.getName());
+        }
+        System.out.println("##########################");
+        System.out.println(lst1.indexOf(0));
     }
 }
