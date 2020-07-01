019916d36cc728fb1ce95e7b02c5cda8f62c148a
emtoa <emtoa@yandex.ru>
2020-07-02 00:03:03 +0300

    test

---
+++ src/main/java/myself/pattern/test5.java
@@ -0,0 +1,14 @@
+package myself.pattern;
+
+import java.util.HashMap;
+
+public class test5 {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("parsentev@yandex.ru", "Petr Arsentev Sergeevich");
+
+        for (String key : map.keySet()) {
+            System.out.println(map.get(key));
+        }
+    }
+}
