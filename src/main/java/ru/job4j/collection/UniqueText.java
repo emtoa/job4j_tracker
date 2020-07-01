897c9dfeb4e26beec33f911cbd84071c396fdbc5
emtoa <emtoa@yandex.ru>
2020-07-01 16:07:41 +0300

    2. Идентичные тексты. [#297927]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -14,7 +14,7 @@ public class UniqueText {
         }
 
         for (String txt : text) {
-            if (!check.contains(txt.toString())){
+            if (!check.contains(txt)){
                 rsl = false;
                 break;
             }
