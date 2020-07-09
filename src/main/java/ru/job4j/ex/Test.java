2b5ce1806f8d1d8a2de32da0d7fe85217534c442
emtoa <emtoa@yandex.ru>
2020-07-09 03:05:24 +0300

    test

--- src/main/java/ru/job4j/ex/Test.java
+++ src/main/java/ru/job4j/ex/Test.java
@@ -1,10 +1,10 @@
 package ru.job4j.ex;
 
-public class test {
+public class Test {
     public static void main(String[] args) {
         try {
             int i = 6 / 0;
-        } catch (Throwable th){
+        } catch (Throwable th) {
             th.printStackTrace();
         }
     }
