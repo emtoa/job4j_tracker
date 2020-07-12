f4677893aae4cdd1fa3cddf1ae1973129f92677a
emtoa <emtoa@yandex.ru>
2020-07-12 12:51:52 +0300

    2.0. Встроенные функциональные интерфейсы. [#297767]

--- src/main/java/ru/job4j/lambda/SearchAtt.java
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -2,36 +2,39 @@ package ru.job4j.lambda;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
+import java.util.function.BiPredicate;
+import java.util.function.BinaryOperator;
+import java.util.function.BooleanSupplier;
+import java.util.function.Predicate;
 
 
 public class SearchAtt {
 
     public static List<Attachment> filterSize(List<Attachment> list) {
-        Function<Attachment, Attachment> func = new Function<Attachment, Attachment>() {
+        Predicate<Attachment> func = new Predicate<Attachment>()  {
             @Override
-            public Attachment apply(Attachment att) {
-                return (att.getSize() > 100) ? att : null;
+            public boolean test(Attachment att) {
+                return (att.getSize() > 100) ? true : false;
             }
         };
         return filter(list, func);
     }
 
     public static List<Attachment> filterName(List<Attachment> list) {
-        Function<Attachment, Attachment> func = new Function<Attachment, Attachment>() {
+        Predicate<Attachment> func = new Predicate<Attachment>() {
             @Override
-            public Attachment apply(Attachment att) {
-                return (att.getName().contains("bug")) ? att : null;
+            public boolean test(Attachment att) {
+                return (att.getName().contains("bug")) ? true : false;
             }
         };
         return filter(list, func);
     }
 
 
-    private static List<Attachment> filter(List<Attachment> lst, Function<Attachment, Attachment> func) {
+    private static List<Attachment> filter(List<Attachment> lst, Predicate<Attachment> func) {
         List<Attachment> rsl = new ArrayList<>();
         for (Attachment att : lst) {
-            if (func.apply(att) != null) {
+            if (func.test(att)) {
                 rsl.add(att);
             }
         }
