528507d8ba223391e8f3603a9f89516e25a1140e
emtoa <emtoa@yandex.ru>
2020-07-13 17:09:23 +0300

    4. Зона видимости в лямбда и исключения [#297763]

--- src/main/java/ru/job4j/lambda/ScopeInside.java
+++ src/main/java/ru/job4j/lambda/ScopeInside.java
@@ -8,9 +8,10 @@ public class ScopeInside {
         int total = 0;
         for (int i = 0; i < number.length; i++) {
             int num = i;
-            total += add(
-                    () -> {
-                        return 1 + num;
+            int inTotal = total;
+            total = add(
+                    () ->  {
+                        return inTotal + num;
                     }
             );
         }
