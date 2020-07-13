c266d7314c03eb6ee249f22b0235d87445d67bce
emtoa <emtoa@yandex.ru>
2020-07-14 02:36:04 +0300

    0. Stream API [#297772]

---
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamUsage {
+    public static void main(String[] args) {
+        List<Integer> tasks = List.of(100, -200, 300, -400);
+
+        List<Integer> result = tasks.stream().filter(task -> task >= 0).collect(Collectors.toList());
+
+        result.forEach(System.out::println);
+    }
+}
