0143adf49e528e3236664521b1bab28765b3a5e5
emtoa <emtoa@yandex.ru>
2020-07-16 16:51:45 +0300

    test

--- src/main/java/myself/pattern/Test17.java
+++ src/main/java/myself/pattern/Test17.java
@@ -1,10 +1,18 @@
 package myself.pattern;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Test17 {
+
+    public static List<Integer> matrixToList(Integer[][] matrix) {
+
+        return Stream.of(matrix)
+                .flatMap(e -> Arrays.stream(e)).collect(Collectors.toList());
+    }
+
     public static void main(String[] args) {
 
         Integer[][] matrix = new Integer[2][2];
@@ -14,12 +22,6 @@ public class Test17 {
         matrix[1][0] = 3;
         matrix[1][1] = 4;
 
-        System.out.println(Stream.of(matrix)
-                .flatMap(e -> Arrays.stream(e)).collect(Collectors.toList()));
-
-        /*
-        System.out.println(Arrays.stream(matrix)
-                .flatMap(e -> Arrays.stream(e)).collect(Collectors.toList()));
-                */
+        System.out.println(matrixToList(matrix));
     }
 }
