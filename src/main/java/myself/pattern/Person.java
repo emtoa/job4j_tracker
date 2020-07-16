de27c2f397f88cd58a97ad40c9ee9ad2daa68f85
emtoa <emtoa@yandex.ru>
2020-07-16 15:06:52 +0300

    4. Преобразование List в Map. [#297776]

---
+++ src/main/java/myself/pattern/Person.java
@@ -0,0 +1,16 @@
+package myself.pattern;
+
+public class Person {
+    private Integer id;
+    private String name;
+    public Person(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+}
--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -1,8 +1,8 @@
 package ru.job4j.stream;
 
-import ru.job4j.search.Person;
+import myself.pattern.Person;
 
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -12,5 +12,23 @@ public class School {
        return students.stream()
                  .filter(predict)
                  .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+
+        List<Student> stud = List.of(
+                new Student("Ivanov", 40),
+                new Student("Petrov", 50),
+                new Student("Sidorov", 71),
+                new Student("Ilin", 49),
+                new Student("Sidorov", 82),
+                new Student("Popov", 69),
+                new Student("Smirnov", 100)
+        );
+
+       LinkedHashMap<String, Student> map = stud.stream()
+                .collect(Collectors.toMap(s -> s.getSurname(), s -> s,
+                        (x, y) -> x, LinkedHashMap::new));
+        map.forEach((x, y) -> System.out.println("Key: " + x +", value: "+ y.getScore()));
     }
 }
