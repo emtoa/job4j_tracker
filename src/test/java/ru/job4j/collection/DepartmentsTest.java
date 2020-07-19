eb22fb244c1c7db68b8d0e97a5ac2b5a65d60734
emtoa <emtoa@yandex.ru>
2020-07-19 23:19:21 +0300

    1. Улучшения Collection API [#297783]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -1,7 +1,6 @@
 package ru.job4j.collection;
 
 import org.junit.Test;
-import java.util.Arrays;
 import java.util.List;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThan;
@@ -10,31 +9,31 @@ import static org.junit.Assert.assertThat;
 public class DepartmentsTest {
     @Test
     public void whenMissed() {
-        List<String> input = Arrays.asList("k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenNonChange3level() {
-        List<String> input = Arrays.asList("k1/sk1/ssk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk1");
+        List<String> input = List.of("k1/sk1/ssk1");
+        List<String> expect = List.of("k1", "k1/sk1", "k1/sk1/ssk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenNonChange() {
-        List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenOrderAsc() {
-        List<String> input = Arrays.asList("a6/ask6/assk6", "k6/sk6/ssk6", "k2/sk2/ssk2");
+        List<String> input = List.of("a6/ask6/assk6", "k6/sk6/ssk6", "k2/sk2/ssk2");
         List<String> result = Departments.fillGaps(input);
         Departments.sortAsc(result);
 
@@ -45,7 +44,7 @@ public class DepartmentsTest {
 
     @Test
     public void whenOrderDesc() {
-        List<String> input = Arrays.asList("a6/ask6/assk6", "k6/sk6/ssk6", "a1", "z1", "k2/sk2/ssk2");
+        List<String> input = List.of("a6/ask6/assk6", "k6/sk6/ssk6", "a1", "z1", "k2/sk2/ssk2");
         List<String> result = Departments.fillGaps(input);
         Departments.sortDesc(result);
 
